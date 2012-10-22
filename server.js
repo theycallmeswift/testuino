c7578cd418c2b43549350143c4535a8fdd07b79e
Swift <theycallmeswift@gmail.com>
2012-10-21 21:46:39 -0400

    Added SendGrid support

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var util = require('util')
   , test_bot = require('./test_bot')
   , Testuino = require('./testuino')
   , testuino = new Testuino
+  , SendGrid = require('sendgrid').SendGrid
+  , sendgrid = new SendGrid(process.env.SENDGRID_USER, process.env.SENDGRID_PASS)
   , express = require('express')
   , app = express()
   , port = process.env.PORT || 3000
@@ -33,7 +35,19 @@ app.post('/github', function(req, res) {
   util.log("Testing commit by " + lastCommit.author.name + " for " + url);
 
   test_bot.test(name, url, lastCommit.id, function(err, pass) {
-    testuino.finish(!err && pass);
+    var passed = (!err && pass)
+      , passText = (passed) ? "PASS" : "FAIL"
+      , text = "You last build for " + name + " just made it through Testuino!  It resulted in a: " + passText + ". \n\r\n\r You can view the commit on github here: \n\r\n\r" + payload.repository.url + "\n\r\n\r Thanks,\n\rThe Testuino Notifier";
+
+    testuino.finish(passed);
+    sendgrid.send({
+      to: lastCommit.author.email,
+      from: 'build.notifier@testuino.com',
+      subject: 'Build status for ' + payload.repository.owner.name + '/' + name,
+      text: text
+    }, function(success, message) {
+      if(!success) util.log(message);
+    });
   });
 
   res.send("OK");
